ace02929e5b0af01125185c9d2c94341a9168c9e
Nick Sorrell <nicksorrell@gmail.com>
2015-04-19 01:00:47 -0400

    Add additional tests and transfer public folder'

---
+++ public/js/client.js
@@ -0,0 +1,44 @@
+'use strict';
+
+$(function(){
+  $.get('/cities', appendToList);
+
+  function appendToList(cities){
+    var list = [];
+    var content, city;
+    for(var i in cities) {
+      city = cities[i];
+      content = '<a href="/cities/' + city + '">' + city + '</a>' + '<a href="#" data-city="' + city + '"><img src="img/del.png"></a>';
+      list.push($('<li>', { html: content }));
+    }
+    $('.city-list').append(list);
+  }
+
+  $('form').on('submit', function(event){
+    event.preventDefault();
+    var form = $(this);
+    var cityData = form.serialize();
+
+    $.ajax({
+      type: 'POST', url: '/cities', data: cityData
+    }).done(function(cityName){
+      appendToList([cityName.toLowerCase()]);
+      form.trigger('reset');
+    });
+  });
+
+  $('.city-list').on('click', 'a[data-city]', function(e){
+    e.preventDefault();
+    if( !confirm('Are you sure you want to remove this city?') ){
+        return false;
+    }
+
+    var target = $(e.currentTarget);
+
+    $.ajax({
+      type: 'DELETE', url: '/cities/' + target.data('city'), data: target.data('city')
+    }).done(function(){
+      target.parents('li').remove();
+    });
+  });
+});
